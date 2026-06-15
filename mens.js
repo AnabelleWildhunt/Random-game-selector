99a13305b04fccd95f9fba3e7b21cce8f6b6d071
Anabelle-of-the-Wildhunt <mak.noemi5@gmail.com>
2026-06-15 19:30:14 +0200

    Added a key function and fixed the typo(graphy)

--- mens.js
+++ mens.js
@@ -1,6 +1,20 @@
 // List
-let games = [
+const games = [
     
 ]
+const gameCover = document.getElementById("game-cover")
+const gameTitle = document.getElementById("game-title")
+const shuffle = document.getElementById("shuffle")
+
 // RNG
-let number = Math.floor(Math.random() * games.length)
+
+function choose(no){
+    gameCover.style.backgroundImage = `url(${games[no].cover})`
+    gameCover.style.backgroundSize = "cover"
+    gameTitle.textContent = `${games[no].title}`
+}
+
+shuffle.addEventListener("click", function(){
+    let number = Math.floor(Math.random() * games.length)
+    choose(number)
+})

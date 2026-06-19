615770f9188038b31290c42ac1877b53f163874b
Anabelle-of-the-Wildhunt <mak.noemi5@gmail.com>
2026-06-19 17:32:10 +0200

    Added CSS, further adjusted the structure and debugged the main js event listener

--- mens.js
+++ mens.js
@@ -2,22 +2,27 @@
 const games = [
     {
         cover: "img/img1",
+        coverFormat: "jpg",
         title: "sample1"
     },
     {
         cover: "img/img2",
+        coverFormat: "jpg",
         title: "sample2"
     },
     {
         cover: "img/img3",
+        coverFormat: "jpg",
         title: "sample3"
     },
     {
         cover: "img/img4",
+        coverFormat: "jpg",
         title: "sample4"
     },
     {
         cover: "img/img5",
+        coverFormat: "jpg",
         title: "sample5"
     }
 ]
@@ -28,7 +33,7 @@ const shuffle = document.getElementById("shuffle")
 // RNG
 
 function choose(no){
-    gameCover.style.backgroundImage = `url(${games[no].cover})`
+    gameCover.style.backgroundImage = `url(${games[no].cover}.${games[no].coverFormat})`
     gameCover.style.backgroundSize = "cover"
     gameTitle.textContent = `${games[no].title}`
 }
